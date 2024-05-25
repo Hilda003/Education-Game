4fb202fce2d8c7bb8e6e98c42126a9df9a9e793a
Hilda003 <Hildatulwardah003@gmail.com>
2024-05-25 16:39:49 +0700

    update dialog

--- Assets/background.cs
+++ Assets/background.cs
@@ -4,27 +4,15 @@ using UnityEngine;
 
 public class BackgroundFollow : MonoBehaviour
 {
-    public Transform character; // Referensi ke transform karakter
-    public Vector3 offset; // Offset untuk posisi latar belakang relatif terhadap karakter
+  
+  public float speed;
 
-    void Start()
-    {
-        if (character == null)
-        {
-            Debug.LogError("Character Transform is not assigned!");
-        }
-    }
+  [SerializeField]
+  private Renderer bgRenderer;
 
-    void Update()
-    {
-        if (character != null)
-        {
-            // Update posisi latar belakang untuk mengikuti karakter
-            Vector3 newPosition = new Vector3(character.position.x + offset.x, character.position.y + offset.y, transform.position.z);
-            transform.position = newPosition;
+  void Update() {
+    bgRenderer.material.mainTextureOffset += new Vector2(speed * Time.deltaTime, 0);
+  }
 
-            Debug.Log("Background Position: " + transform.position);
-            Debug.Log("Character Position: " + character.position);
-        }
-    }
+
 }
